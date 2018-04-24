035c864c99dcd4e816c56c6e311d21241db249de
julio <julio.ola6@gmail.com>
2018-04-24 20:52:38 -0300

    v2: agregado un timer

--- heck.py
+++ heck.py
@@ -1,1 +1,7 @@
 #v1: solo subida para probar el git push
+print ("hola bienvenido a su fin")
+print ("le recomiendo que guarde cualquier archivo")
+time.sleep(10)
+prin("terminado")
+#v2: introduje un timer para que no se ejecute de inmediato
+
