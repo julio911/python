3ec1921fe38a7515a6e5026906b82a2b4cdbde1b
julio <julio.ola6@gmail.com>
2018-04-24 21:53:26 -0300

    arreglados pequeños errores

--- heck.py
+++ heck.py
@@ -1,7 +1,9 @@
 #v1: solo subida para probar el git push
+import time
+
 print ("hola bienvenido a su fin")
 print ("le recomiendo que guarde cualquier archivo")
 time.sleep(10)
-prin("terminado")
+print("terminado")
 #v2: introduje un timer para que no se ejecute de inmediato
 
